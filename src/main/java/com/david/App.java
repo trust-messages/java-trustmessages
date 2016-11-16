0a658b275670fcb87c56ee87aa3d8c3496b0ab2c
David Jelenc <david.jelenc@fri.uni-lj.si>
2016-11-16 15:19:38 +0100

    Fixes compile issues

--- src/main/java/com/david/App.java
+++ src/main/java/com/david/App.java
@@ -1,15 +1,36 @@
 package com.david;
 
-import org.openmuc.jasn1.ber.BerIdentifier;
+import com.david.format.QTM;
+import com.david.messages.*;
+import org.openmuc.jasn1.ber.BerByteArrayOutputStream;
+import org.openmuc.jasn1.ber.types.BerAny;
+
+import java.io.IOException;
+import java.util.Base64;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final Trust trust = new Trust();
         trust.date = new BinaryTime(System.currentTimeMillis());
-        trust.id = new BerIdentifier();
-        trust.service = new Service("seller".getBytes());
-        trust.target = new Entity("david@fri.si".getBytes());
+        trust.service = new Service("seller");
+        trust.target = new Entity("david@fri.si");
+        trust.tms = new SystemIdentity(1, 2, 3);
 
-        System.out.println(trust);
+        final QTM v = new QTM();
+        v.value = 0;
+
+        v.encodeAndSave();
+
+        trust.value = new BerAny(v.code);
+
+        final BerByteArrayOutputStream baos = new BerByteArrayOutputStream(32, true);
+
+        trust.encode(baos, true);
+
+        final String base64 = Base64.getEncoder().encodeToString(baos.getArray());
+
+        System.out.println(base64);
     }
+
+
 }
