f935d805a959eb9608ac075f9209aafc9fe39193
David Jelenc <david.jelenc@fri.uni-lj.si>
2016-11-18 15:47:55 +0100

    Starting work on TrustSocket

---
+++ src/main/java/com/david/TrustSocket.java
@@ -0,0 +1,7 @@
+package com.david;
+
+public class TrustSocket {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
--- src/main/java/com/david/Utils.java
+++ src/main/java/com/david/Utils.java
@@ -3,23 +3,34 @@ package com.david;
 import com.david.antlr.QueryLexer;
 import com.david.antlr.QueryParser;
 import com.david.antlr.Visitor;
+import com.david.messages.AssessmentRequest;
+import com.david.messages.Message;
 import com.david.messages.Query;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.openmuc.jasn1.ber.BerByteArrayOutputStream;
+import org.openmuc.jasn1.ber.types.BerInteger;
 
 import java.io.IOException;
 import java.util.Base64;
 
 public class Utils {
-    private final static Base64.Decoder DECODER = Base64.getDecoder();
-
     public static byte[] decode(String in) {
-        return DECODER.decode(in);
+        return Base64.getDecoder().decode(in);
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
+        final AssessmentRequest ar = new AssessmentRequest(new BerInteger(1),
+                getQuery("source = david AND (service = seller OR service = letter) AND (target = balu OR target = aleks)"));
+        final Message m = new Message(ar, null, null, null, null, null, null);
+
+        final BerByteArrayOutputStream baos = new BerByteArrayOutputStream(100, true);
+        m.encode(baos, true);
+
+        System.out.println(Base64.getEncoder().encodeToString(baos.getArray()));
+        System.out.println(m);
+        m.
     }
 
     public static Query getQuery(String query) {
